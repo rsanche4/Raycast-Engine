9f34a440261d91b37094aa671aea91d86f038eba
Rafael Sanchez <rsanzek25@gmail.com>
2024-12-05 00:04:43 -0500

    added some projects

---
+++ MyProjects/Girl and Zombies/src/myGamePack/Sound.java
@@ -0,0 +1,54 @@
+package myGamePack;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+  
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Sound {
+
+	// the sound class. Keep files to .wav
+    private static Clip clip;
+	
+	public static synchronized void playSound(String file_path, boolean loop) {
+		// create AudioInputStream object
+		AudioInputStream audioInputStream;
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(new File(file_path).getAbsoluteFile());
+			
+	        clip = AudioSystem.getClip();
+	        
+	     // open audioInputStream to the clip
+	        clip.open(audioInputStream);
+	        if (loop) {
+	        	clip.loop(Clip.LOOP_CONTINUOUSLY);
+	        }
+	        clip.start();
+		} catch (UnsupportedAudioFileException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			
+			e.printStackTrace();
+		}
+          
+        
+	}
+	public static synchronized void stopSound() {
+		try 
+		{
+			clip.stop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Welcome to the **Raycast Engine**! This project is a lightweight and simple 3D rendering engine built around the concept of raycasting, which simulates the way light travels and interacts with objects in a 3D environment.
 
-The Raycaster Engine is typically used in first-person perspective games or simulations and can render walls, floors, and ceilings, creating the illusion of a 3D world on a 2D screen. Despite its simplicity, this engine allows for the creation of dynamic and immersive experiences with basic 3D elements like textures, lighting effects, and object placement.
+The Raycast Engine is typically used in first-person perspective games or simulations and can render walls, floors, and ceilings, creating the illusion of a 3D world on a 2D screen. Despite its simplicity, this engine allows for the creation of dynamic and immersive experiences with basic 3D elements like textures, lighting effects, and object placement.
 
 In this document, you will find Installation guides, API specifications, and how to code your own games using this engine!
 
